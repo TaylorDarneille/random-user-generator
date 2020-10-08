62c64a841734262f8f7dec70aa012baa24a89e34
Taylor Darneille <33237160+TaylorDarneille@users.noreply.github.com>
2020-10-07 19:13:09 -0700

    updated for FLEX 818 code-along

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-const requestUrl = "https://randomuser.me/api/?results=";
+const requestUrl = "https://randomuser.me/api/?results="
 
-document.addEventListener("DOMContentLoaded", ()=>{
+const addPerson = (person) => {
+    // create a new li
+    let li = document.createElement("li")
+    // add the first and last name of the person to the li
+    li.textContent = `${person.name.first} ${person.name.last}  `
+    let pic = document.createElement("img")
+    pic.setAttribute("src", person.picture.thumbnail)
+    li.appendChild(pic)
+    // append the li to the ul
+    peopleList.appendChild(li)
+}
 
-    form.addEventListener('submit', (e)=>{
-        e.preventDefault()
-        fetchPeople(requestUrl+input.value)
+const fetchPeople = () => {
+    fetch(requestUrl+input.value)
+    .then((responseData)=>{
+        return responseData.json()
+    })
+    .then((jsonData)=>{
+        console.log("jsonData.results:", jsonData.results)
+        // add a li to the ul for each person in the jsonData
+        let people = jsonData.results
+        people.forEach(addPerson)
+    })
+    .catch((err)=>{
+        console.log("SOS THERE IS AN ERROR: ", err)
+    })
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    form.addEventListener('submit', (event)=>{
+        event.preventDefault()
+        while(peopleList.firstChild){
+            peopleList.removeChild(peopleList.firstChild)
+        }
+        fetchPeople(input.value)
     })
 
-    const fetchPeople = (endpoint) => {
-        fetch(endpoint)
-        .then((responseData)=>{
-            return responseData.json();
-        })
-        .then((jsonData)=>{
-            let people = jsonData.results;
-            while(peopleList.firstChild) {
-                peopleList.removeChild(peopleList.firstChild)
-            }
-            people.forEach(addPerson);
-        })
-        .catch((error)=>{
-            console.log("error!!!:", error);
-        });
-    }
-
-    const addPerson = (person)=>{
-        let li = document.createElement("li");
-        li.textContent = person.name.first, person.name.last;
-        peopleList.appendChild(li);
-    }
-
-});
+})
